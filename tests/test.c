eea302d6666fcd1130296bb2bc67c44d1f2d44f7
Andreyko <Andreyko@ANDY>
2024-03-14 19:19:09 +0700

    Написал три модульных теста для функции solve_equation

--- tests/test.c
+++ tests/test.c
@@ -1,9 +1,28 @@
 #include <check.h>
 #include "../src/header.h"
 
-START_TEST(checking_tests_for_correctness)
+START_TEST(checking_for_missing_roots)
 {
-    fail_unless(5 == 5, "this should be successful");
+    struct roots example = solve_equation(2, 1, 2);
+    fail_unless(example.size == 0, "There shouldn't be a single root here.");
+}
+END_TEST
+
+START_TEST(checking_for_one_root)
+{
+    struct roots example = solve_equation(2, 4, 2);
+    fail_unless(example.size == 1, "There should be one root here");
+    fail_unless(example.x[0] == -1, "The root must be -1");
+    
+}
+END_TEST
+
+START_TEST(checking_for_two_root)
+{
+    struct roots example = solve_equation(2, 3, 1);
+    fail_unless(example.size == 2, "There should be two roots here");
+    fail_unless(example.x[0] == -0.5, "The first root must be -0.5");
+    fail_unless(example.x[1] == -1, "The second root must be -1");
 }
 END_TEST
 
@@ -16,9 +35,9 @@ int main(void)
     int nf;
 
     suite_add_tcase(s1, tc1_1);
-    tcase_add_test(tc1_1, checking_tests_for_correctness);
-    //tcase_add_test(tc1_1, ñhecking_tests_for_correctness);
-    //tcase_add_test(tc1_1, ñhecking_tests_for_correctness);
+    tcase_add_test(tc1_1, checking_for_missing_roots);
+    tcase_add_test(tc1_1, checking_for_one_root);
+    tcase_add_test(tc1_1, checking_for_two_root);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
