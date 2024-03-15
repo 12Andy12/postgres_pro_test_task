be212173c93c66189c82ade37883b24d3ec4f2be
Andreyko <Andreyko@ANDY>
2024-03-15 15:02:51 +0700

    Изменил подход к нахождению корней, избавился от не нужных условий

--- src/quadratic_equation.c
+++ src/quadratic_equation.c
@@ -6,24 +6,20 @@ struct roots solve_equation(int a, int b, int c)
 
 	double discriminant = b * b - 4 * a * c; //Вычисляем дискриминант
 
-	if (a == 0 || discriminant < 0) // Корней нет или уравнение не квадратное
-	{
-		result.size = 0;
-	}
-	else if (discriminant == 0) // Один корень
-	{
-		result.size = 1;
-		result.x = calloc(1, sizeof(double));
+	if (a == 0 || discriminant < 0)
+		result.size = 0; // Корней нет или уравнение не квадратное
+	else if (discriminant == 0) 
+		result.size = 1; // Один корень
+	else 
+		result.size = 2; // Два корня
 
-		result.x[0] = -b / (2 * a);
-	}
-	else if (discriminant > 0) // Два корня
-	{
-		result.size = 2;
-		result.x = calloc(2, sizeof(double));
+	result.x = calloc(result.size, sizeof(double));
+	int sign = 1;
 
-		result.x[0] = (-b + sqrt(discriminant)) / (2 * a);
-		result.x[1] = (-b - sqrt(discriminant)) / (2 * a);
+	for (unsigned int i = 0; i < result.size; ++i)
+	{
+		result.x[i] = (-b +(sign * sqrt(discriminant))) / (2 * a);
+		sign *= -1;
 	}
 
 	return result;
